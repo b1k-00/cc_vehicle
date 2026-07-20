608599a5384068224c359fbabe9fd71a9377ab86
bc <bc@DESKTOP-VJASLA9.localdomain>
2026-07-19 19:45:12 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+##main py file
+
+
+## daashboard reading
+
+
+coolant_temp = 210
+oil_temp = 210
+oil_pressure = 220
+transmission_temp = 180
+
+print("Coolant Temp:", coolant_temp)
+print("Oil Temp:", oil_temp)
+print("Oil Pressure", oil_pressure)
+print("Transmission Temp:", transmission_temp)
